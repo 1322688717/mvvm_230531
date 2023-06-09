857d8bad04ff496968bb83074e086cd8dd8b8cb0
zhaichaoqun <81673615+1322688717@users.noreply.github.com>
2023-06-09 09:59:11 +0800

    add: code

--- app/src/main/java/com/example/mvvmkotlin/MyBroadcastReceiver.kt
+++ app/src/main/java/com/example/mvvmkotlin/MyBroadcastReceiver.kt
@@ -3,15 +3,16 @@ package com.example.mvvmkotlin
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import com.example.mvvmkotlin.ui.MainActivity
-
+import android.util.Log
+import com.example.mvvmkotlin.ui.HomeActivity
 
 class MyBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
+        Log.e("TAG", "Intent=$intent")
         // 处理接收到的广播事件
         if (intent.action == "com.example.MY_ACTION") {
             // 启动目标 Activity
-            val targetIntent = Intent(context, MainActivity::class.java)
+            val targetIntent = Intent(context, HomeActivity::class.java)
             targetIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
             context.startActivity(targetIntent)
         }
--- app/src/main/java/com/example/mvvmkotlin/ui/MainActivity.kt
+++ app/src/main/java/com/example/mvvmkotlin/ui/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.mvvmkotlin.ui
 
+import android.content.IntentFilter
 import android.os.Bundle
-import android.util.Log
+import com.example.mvvmkotlin.MyBroadcastReceiver
 import com.example.mvvmkotlin.base.BaseActivity
 import com.example.mvvmkotlin.databinding.ActivityMainBinding
-import com.gyf.immersionbar.ktx.immersionBar
 import me.hgj.mvvmhelper.base.BaseViewModel
 
+
 class MainActivity : BaseActivity<BaseViewModel, ActivityMainBinding>() {
+
     override fun initView(savedInstanceState: Bundle?) {
+        val receiver = MyBroadcastReceiver()
+        val intentFilter = IntentFilter("com.example.MY_ACTION")
+        registerReceiver(receiver, intentFilter)
 
     }
 }
